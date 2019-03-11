83927469dbb70b7821954bb135b4e5082b206ee2
abanoubtalaatr <abanoubtalaat50@gmail.com>
2019-03-12 01:00:21 +0200

    add page of reset_password

---
+++ html/reset_password.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="form_log_in">
+			<form>
+				  <div class="form-gruop">
+				  	<h2>Reset Password</h2>
+				  </div>
+				  
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Password</label>
+				    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="New Password">
+				  </div>
+
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Rewrite Password</label>
+				    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Rewirte New Password">
+				  </div>
+				  
+				  <button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div> <!-- End the form-log_in  -->
+	</div> <!-- End the container -->
+</body>
+</html>
