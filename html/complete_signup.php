9a2cd97a4d96ff3394665f564f125882dd7d20ab
bassem55 <44035521+bassem55@users.noreply.github.com>
2019-03-24 18:14:07 +0200

    Add files via upload

---
+++ html/complete_signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="form_log_in">
+			<form>
+				  <div class="form-gruop">
+				  	<h2>Complete Signup</h2>
+				  </div>
+				  
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Your Code you have been recived on Your Email</label>
+				    <input type="text" class="form-control" id="code" placeholder="Code">
+				  </div>
+				   <button type="submit" class="btn btn-primary" id='check_code'>Chek</button>
+
+			</form>
+
+		</div> <!-- End the form-log_in  -->
+	</div> <!-- End the container -->
+	<script type="text/javascript" src="../js/complete_signup.js"></script>
+</body>
+</html>
