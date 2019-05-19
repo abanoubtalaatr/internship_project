613b8eb206bda74fcd9afdc68ebf9912e19045f0
bassem55 <44035521+bassem55@users.noreply.github.com>
2019-05-19 18:37:16 +0200

    Update tasks_correction.php

--- php/tasks_correction.php
+++ php/tasks_correction.php
@@ -1,9 +1,10 @@
 <?php 
 class task_soluation
 {
-    public function task_soluation()
+    private $con;
+    public function task_soluation($server_name , $db_user , $db_pass , $db_name)
     {
-
+        $this->con = new mysqli($server_name , $db_user , $db_pass , $db_name);
     }
     public function correct_this($task_number , $id)
     {
@@ -18,19 +19,41 @@ class task_soluation
            $degree =  $this->task_1_correction($path_file);
            $user_degree = $degree[0];
            $total_degree = $degree[1];
-           //next step store degree into database
+           
+           $update = "UPDATE tasks_degree SET task_1_degree = '".$user_degree."' , task_1_total_degree = '".$total_degree."'  WHERE  id = '".$id."' ;
+        
+           $this->con->query($update);
         }
         else if($task_number == 2)
         {
             $degree = $this->task_2_correction($path_file);
             $user_degree = $degree[0];
             $total_degree = $degree[1];
+
+            $update = "UPDATE tasks_degree SET task_2_degree = '".$user_degree."' , task_2_total_degree = '".$total_degree."'  WHERE  id = '".$id."' ;
+        
+           $this->con->query($update);
+            
         }
         else if($task_number == 3)
         {
             $degree = $this->task_3_correction($path_file);
             $user_degree = $degree[0];
             $total_degree = $degree[1];
+
+            $update = "UPDATE tasks_degree SET task_3_degree = '".$user_degree."' , task_3_total_degree = '".$total_degree."'  WHERE  id = '".$id."' ;
+        
+           $this->con->query($update);
+        }
+        else if($task_number == 4)
+        {
+            $degree = $this->task_4_correction($path_file);
+            $user_degree = $degree[0];
+            $total_degree = $degree[1];
+
+            $update = "UPDATE tasks_degree SET task_4_degree = '".$user_degree."' , task_4_total_degree = '".$total_degree."'  WHERE  id = '".$id."' ;
+        
+           $this->con->query($update);
         }
     }
     /*
@@ -192,6 +215,100 @@ class task_soluation
         return $arr;
 
     }
+    /*
+        use last tasks to create function signup that his job
+        make validate for inputs and store it if vaild
+
+        create class name task that have 
+        function name signup that will take four parameters
+        ==>first_name
+        ==>last_name
+        ==>email
+        ==>password
+
+        make validation for all parameters
+
+        if everything good  return correct
+
+        if atleast one parameter wrong return wrong
+
+        
+    */
+    public function task_4_correction($path_file)
+    {
+        require_once $path_file;
+
+        $task = new task("localhost","root" , "" , "company");
+
+        $correct_name = array();
+        $correct_name[0] = "abanoub";
+        $correct_name[1] = "ali";
+        $correct_name[2] = "kero";
+        $correct_name[3] = "bassem";
+        $correct_name[4] = "namewithoutspace";
+        $correct_name[5] = "namewithouttag";
+
+        $correct_email = array();
+        $correct_email[0] = "bassemreda55@gmail.com";
+        $correct_email[1] = "abanoub@yahoo.com";
+        $correct_email[2] = "bassem@yahoo.com";
+        $correct_email[3] = "abanoub@gmail.com";
+        $correct_email[4] = "kero@yahoo.com";
+        $correct_email[5] = "marco@gmail.com";
+
+        $correct_password = array();
+        $correct_password[0] = "bassem12";
+        $correct_password[1] = "bassem12345";
+        $correct_password[2] = "<bassemreda55>";
+        $correct_password[3] = "<script>bassem55</scropt>";
+        $correct_password[4] = "1234bassem34";
+        $correct_password[5] = "abanoub<>s55";
+
+        $wrong_name = array();
+        $wrong_name[0] = "";
+        $wrong_name[1] = "al";
+        $wrong_name[2] = "asdryuimngyuioplkjhgd";
+        $wrong_name[3] = "bassem55";
+        $wrong_name[4] = "bassem reda";
+        $wrong_name[5] = "<script>anything</script>";
+
+        $wrong_email = array();
+        $wrong_email[0] = "";//empty
+        $wrong_email[1] = "bassem reda@gmail.com";//space
+        $wrong_email[2] = "bassemreda@anything.com";//not gmail or yahoo
+        $wrong_email[3] = "bassem#gmail.com";//# not @
+        $wrong_email[4] = "bassem@gmail.com@gmail.com";//@gmail.com written two times
+        $wrong_email[5] = "bassem@gmailcom";
+
+        $wrong_password = array();
+        $wrong_password[0] = "12345678";//do not have char
+        $wrong_password[1] = "basemreda";//do not have a number
+        $wrong_password[2] = "bassem5";//7 digit
+        $wrong_password[3] = "";
+        $wrong_password[4] = "marco";
+        $wrong_password[5] = "<><><>";
+
+        $degree = 0;
+        for($i=0;$i<count($wrong_email);$i++)
+        {
+            $result_correct = $task->signup($correct_name[$i] , $correct_name[$i] , $correct_email[$i] , $correct_password[$i]);
+            $result_wrong   = $task->signup($wrong_name[$i] , $wrong_name[$i] , $wrong_email[$i] , $wrong_password[$i]);
+
+            if($result_wrong == "wrong" && $result_correct == "correct")
+            {
+                $degree++;
+            }
+            else if($result_wrong == "correct" || $result_correct == "wrong")
+            {
+                continue;
+            }
+        }
+        $arr = array();
+        $arr[0] = $degree;//degree
+        $arr[1] = count($wrong_email);//total degree 
+        return $arr;
+       
+    }
 
 }
 ?>
@@ -200,7 +317,7 @@ class task_soluation
     {
         if($_POST['fun'] == "check_task")
         {
-            $task = new task_soluation();
+            $task = new task_soluation("localhost" , "root" , "" , "project");
             $task->correct_this($_POST['task_number'] , $_POST['id']);
         }
     }
