195dd5e3e2ea34b323ee0946581c2e76a1b5674f
bassem55 <44035521+bassem55@users.noreply.github.com>
2019-06-06 19:05:18 +0200

    Update tasks_correction.php

--- php/tasks_correction.php
+++ php/tasks_correction.php
@@ -107,7 +107,7 @@ class task_soluation
         else if($task_number == 4)
         {
 
-            $degree = $this->task_4_correction($path_file);
+            $degree = $this->task_4_correction($path_file , $id);
             $user_degree = $degree[0];
             $total_degree = $degree[1];
             $error = $degree[2];
@@ -330,26 +330,35 @@ class task_soluation
         use last tasks to create function signup that his job
         make validate for inputs and store it if vaild
 
-        create class name task that have 
-        function name signup that will take four parameters
+        create class name task that have four parameters (servername , username_db , password_db , db_name)
+        make connection with database to make some querys .
+        and create  function name signup that will take four parameters
         ==>first_name
         ==>last_name
         ==>email
+        ==>phone_number
         ==>password
+        ==>repassword
+        ==>age (any age) 
+        ==>country (any country)
+        ==>table name that you will store data there
+
 
         make validation for all parameters
 
-        if everything good  return correct
-
+        if everything good store all data on database that have table name (the last parameter)
+        with columes name have the same name of parameters that we asked from you but sure do not create colum repassword and table name
+        and return correct
+        
         if atleast one parameter wrong return wrong
 
         
     */
-    private function task_4_correction($path_file)
+    private function task_4_correction($path_file , $id)
     {
         require_once $path_file;
 
-        $task = new task();
+        $task = new task("localhost" , "root" , "" , "project");
 
         $correct_name = array();
         $correct_name[0] = "abanoub";
@@ -366,6 +375,15 @@ class task_soluation
         $correct_email[3] = "abanoub@gmail.com";
         $correct_email[4] = "kero@yahoo.com";
         $correct_email[5] = "marco@gmail.com";
+
+        $correct_phone = array();
+
+        $correct_phone[0] = "01201873616";
+        $correct_phone[1] = "01102764837";
+        $correct_phone[2] = "01098237673";
+        $correct_phone[3] = "01201873616";
+        $correct_phone[4] = "01102764837";
+        $correct_phone[5] = "01098237673";
 
         $correct_password = array();
         $correct_password[0] = "bassem12";
@@ -391,6 +409,15 @@ class task_soluation
         $wrong_email[4] = "bassem@gmail.com@gmail.com";//@gmail.com written two times
         $wrong_email[5] = "bassem@gmailcom";
 
+        $wrong_phone = array();
+        $wrong_phone[0] = "0120287465";//10 
+        $wrong_phone[1] = "012028736166";//12
+        $wrong_phone[2] = "012012";//6
+        $wrong_phone[3] = "01302873616";//12 but start with 013
+        $wrong_phone[4] = "01702873616";//12 but start with 017
+        $wrong_phone[5] = "012e45mu576";//12 but contain characters
+
+
         $wrong_password = array();
         $wrong_password[0] = "12345678";//do not have char
         $wrong_password[1] = "basemreda";//do not have a number
@@ -399,41 +426,212 @@ class task_soluation
         $wrong_password[4] = "marco";
         $wrong_password[5] = "<><><>";
 
+        //we will create table to test if data stored or not
+        $create = "CREATE TABLE signup_" . $id . " (
+            first_name varchar(20) ,
+            last_name  varchar(20) ,
+            email      varchar(40) ,
+            phone_number INT(11) ,
+            password varchar(20) , 
+            age INT(2),
+            country varchar(20)
+            )";
+        $this->con->query($create);
+
         $degree = 0;
         for($i=0;$i<count($wrong_email);$i++)
         {
-            $result_correct = $task->signup($correct_name[$i] , $correct_name[$i] , $correct_email[$i] , $correct_password[$i]);
-            $result_wrong   = $task->signup($wrong_name[$i] , $wrong_name[$i] , $wrong_email[$i] , $wrong_password[$i]);
+            $result_correct = $task->signup($correct_name[$i] , $correct_name[$i] , $correct_email[$i] ,$correct_phone , $correct_password[$i] , $correct_password , "21" , "egypt" , "signup_".$id);
+            $result_wrong   = $task->signup($wrong_name[$i] , $wrong_name[$i] , $wrong_email[$i] ,$wrong_phone, $wrong_password[$i] , $wrong_password , "0" , "egypt" , "signup_".$id);
+
 
             $notes = array();
             $counter = 0;
-            if($result_wrong == "wrong")
+            if($result_wrong === "wrong")
             {
-                $degree++;
+                //here we will check if data stored 
+                //if stored so it is error
+
+                $select = "SELECT first_name , last_name , email , phone_number , password , age , country FROM signup_" .$id ;
+                $res = $this->con->query($select);
+                if($res->num_rows > 0)
+                {
+                    $notes[$counter] = "data wrong and you store it on database";
+                    $counter++;
+                }
+                else
+                {
+                    $degree++;
+                }
+                
             }
-            else
+            else if($result_wrong === "correct")
             {
-                $notes[$counter] =  "(".$wrong_name[$i]." , ".$wrong_name[$i]." , ".$wrong_email[$i]."  , ".$wrong_password[$i].")";
+                $notes[$counter] =  "(".$wrong_name[$i]." , ".$wrong_name[$i]." , ".$wrong_email[$i]."  , ".$wrong_phone[$i]." , ".$wrong_password." , ".$wrong_password." , 0 , egypt)";
                 $counter++;
             }
 
-            if($result_correct == "correct")
+            if($result_correct === "correct")
+            {
+                //here we will check if data stored or not
+                //first we will create table name signup_id
+                $select = "SELECT first_name , last_name , email , phone_number , password , age , country FROM signup_" .$id ;
+                $res = $this->con->query($select);
+                if($res->num_rows == 1)
+                {
+                    while($row = $res->fetch_assoc())
+                    {
+                        if($row['first_name'] == $correct_name[$i] && $row['last_name'] == $correct_name[$i] && $row['email'] == $correct_email[$i] && $row['phone_number'] == $correct_phone[$i] && $row['password'] == $correct_password[$i] && $row['age'] == "21" && $row['country'] == "egypt")
+                        {
+                            $degree++;
+                        }
+                    }
+                }
+                else
+                {
+                    $notes[$counter] = "data correct and you do not store it";
+                    $counter++;
+                }
+                
+            }
+            else if($result_correct === "wrong")
+            {
+                $notes[$counter] =  "(".$correct_name[$i]." , ".$correct_name[$i]." , ".$correct_email[$i]."  , ".$correct_phone[$i]." , ".$correct_password." , ".$correct_password." , 21 , egypt)";
+                $counter++;
+            }
+            
+        }
+        //after we correct the task we will delete the table we created
+        $delete = "DROP TABLE signup_" . $id ;
+        $this->con->query($delete);
+        $arr = array();
+        $arr[0] = $degree;//degree
+        $arr[1] = count($result_correct) + count($result_wrong);//total degree 
+        $arr[2] = $notes;
+        return $arr;
+       
+    }
+    /*
+        task number 5 
+        login
+        create class name task that have four parameters (servername , username_db , password_db , db_name)
+        make connection with database to make some querys .
+        create function name login that have four parameters
+        ==>username
+        ==>password
+        ==>table name (that have usernames and passwords)
+
+        username maybe email or phone number
+        so you should know if username written is email or phone number to search about it on database
+
+        table name have 3 columes (email , phone_number , password)
+
+        return correct if you found username and password on database
+
+        return wrong if you do not found the username and password
+
+        return unknown if user  enter non email or  non phone number like(bassemreda)
+
+    */
+    private function task_5_correction($path_file , $id)
+    {
+        require_once $path_file;
+        //first we will create table login
+        $create = "CREATE TABLE login_" . $id . " (
+            email varchar(40) ,
+            phone_number  INT(11) ,
+            password varchar(20) 
+            )";
+        $this->con->query($create);
+
+        //secand we will add some data into table to test it
+        $insert1 = "INSERT INTO login_" . $id . " (email , password) VALUES ('bassemreda55@gmail.com' , 'bassem12345')";
+        $insert2 = "INSERT INTO login_" . $id . " (phone_number , password) VALUES ('01202873616' , 'bassem123456')";
+        $insert3 = "INSERT INTO login_" . $id . " (email , password) VALUES ('bassemreda55@yahoo.com' , 'bassem1234567')";
+
+        $this->con->query($insert1);
+        $this->con->query($insert2);
+        $this->con->query($insert3);
+
+        $task = new task('localhost' , 'root' , '' , 'project');
+
+        $correct_username = array();
+        $correct_username[0] = "bassemreda55@gmail.com";
+        $correct_username[1] = "01202873616";
+        $correct_username[2] = "bassemreda55@yahoo.com";
+
+        $correct_password = array();
+        $correct_password[0] = "bassem12345";
+        $correct_password[1] = "bassem123456";
+        $correct_password[2] = "bassem1234567";
+
+        $wrong_username = array();
+        $wrong_username[0] = "abanoub@gmail.com";
+        $wrong_username[1] = "marco@yahoo.com";
+        $wrong_username[2] = "01009876567";
+
+        $wrong_password = array();
+        $wrong_password[0] = "bassem12345";
+        $wrong_password[1] = "bassem123456";
+        $wrong_password[2] = "bassem1234567";
+
+        
+        $degree = 0;
+        $notes = array();
+        $counter = 0;
+        for($i=0;$i<count($correct_password);$i++)
+        {
+            $result_correct = $task->login($correct_username[$i] , $correct_password[$i] , "login_" . $id);
+            $result_wrong = $task->login($wrong_username[$i] , $wrong_password[$i] , "login_" . $id);
+
+            if($result_correct === "correct")
             {
                 $degree++;
             }
             else if($result_correct == "wrong")
             {
-                $notes[$counter] =  "(".$correct_name[$i]." , ".$correct_name[$i]." , ".$correct_email[$i]."  , ".$correct_password[$i].")";
+                $notes[$counter] = "you do not return correct although we type username and password stored on database";
                 $counter++;
             }
-            
+            else if($result_correct === "unknown")
+            {
+                $notes[$counter] = "you return unknown although we type correct username";
+                $counter++;
+            }
+            else
+            {
+                $notes[$counter] = "you do not return expected result";
+                $counter++;
+            }
+            if($result_wrong === "wrong")
+            {
+                $degree++;
+            }
+            else if($result_wrong === "correct")
+            {
+                $notes[$counter] = "you do not return wrong although we type username and password not exist on database";
+                $counter++;
+            }
+            else if($result_wrong === "unknown")
+            {
+                $notes[$counter] = "you return unknown although we type correct username";
+                $counter++;
+            }
+            else
+            {
+                $notes[$counter] = "you do not return expected result";
+                $counter++;
+            }
         }
+
+        $delete = "DROP TABLE login_" . $id ;
+        $this->con->query($delete);
         $arr = array();
         $arr[0] = $degree;//degree
-        $arr[1] = count($wrong) + count($correct);//total degree 
+        $arr[1] = count($result_wrong) + count($result_correct);//total degree 
         $arr[2] = $notes;
         return $arr;
-       
+        
     }
 
 }
